fa68382d45b43fa935050d5ca95d8c9b9a84793d
tayyip <mycankaya@gtu.edu.tr>
2016-10-25 18:21:30 +0300

    ...

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,14 +1,31 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
-
+#include "qmessagebox.h"
+#include <cstring>
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_lineEdit_returnPressed()
+{
+
+    QString gelenDeger=ui->lineEdit->text();
+    QStringList lines =gelenDeger.split(" ");
+
+   // QString Satir0 = lines.at(0);
+    //QString Satir1 = lines.at(1);
+
+     QMessageBox::information(this,"baslık",QString::number(lines.count()));
+    ui->textBrowser->insertPlainText(QString::number(lines.count()));
+   // ui->textBrowser->setText(Satir0+" fonksiyonuma "+Satir1+" parametresi gonderiliyor\n");
+    ui->lineEdit->setText("");
 }
