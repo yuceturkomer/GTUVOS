eafeeb955440c200fc5d0521cd97d38646e97b1f
Mustafa Akıllı <makilli@gtu.edu.tr>
2016-11-01 20:51:42 +0300

    Implementation of Function CopyFile
    CopyFile function's prototype was changed:
    - int copyFile(string filePath, string targetPath, int ifTheFileAlreadyExists, string* theNameOfTheCopyFileCreated)
    
    So CopyFile function have a new features. All exceptions are checked. Now, there is more options for already exist same file name in the target path.
    
    There are helper functions for CopyFile funtion:
    -bool isItAFile(string filePath)
    -bool isItADirectory(string dirPath)
    -void findTheFileNameInGivenPath(string filePath, string* theNameOfTheFileToBeCopied)
    -void mergethePathAndFileName(string Path, string fileName, string* newPath)
    -bool copyFromSourceFileToDestinationFile(int sourceFile, int destinationFile)
    -bool findTheUniqueNameInThePath(string* filePath)
    
    Resource for send file:
    http://stackoverflow.com/questions/10195343/copy-a-file-in-a-sane-safe-and-efficient-way

--- gtuvos.h
+++ gtuvos.h
@@ -5,6 +5,16 @@
 #include <string>
 #include "mailserver.h"
 
+// begin of the library about copyFile function
+#include <iostream>
+#include <sys/sendfile.h>  // sendfile
+#include <fcntl.h>         // open
+#include <unistd.h>        // close
+#include <sys/stat.h>      // fstat
+#include <sys/types.h>     // fstat
+#include <dirent.h>        // DIR
+// end of the library about copyFile function
+
 using std::vector;
 using std::string;
 
@@ -42,14 +52,82 @@ public:
     /*
      * Copy the file to the target path
      *
-     * -Copy the file to the target path
-     * -If target path have a file which have a same name like given file name
-     * then change the given file name.
+     * SUCCESSFUL CASES:
      *
-     * @param file Path
-     * @param Target Path
+     * If target path have not a file which have a same name like given file name then:
+     *      -if the file copy process happens, then return SUCCESS_COPY_FILE.
+     *
+     * If target path have a file which have a same name like given file name then:
+     *      -if ifTheFileAlreadyExists IF_THE_FILE_ALREADY_EXISTS_DO_NOTHING, then do nothing and return IF_THE_FILE_ALREADY_EXISTS_DO_NOTHING
+     *      -if ifTheFileAlreadyExists IF_THE_FILE_ALREADY_EXISTS_CHANGE_FILE_NAME, then change the file name which will created and return IF_THE_FILE_ALREADY_EXISTS_CHANGE_FILE_NAME
+     *      -if ifTheFileAlreadyExists IF_THE_FILE_ALREADY_EXISTS_OVERWRITE, then overwrite the existing file and return IF_THE_FILE_ALREADY_EXISTS_OVERWRITE
+     *
+     *
+     * UNSUCCESSFUL CASES:
+     *
+     * If the file copy process fails, then return FAIL_COPY_FILE.
+     * If filePath (include file name) is not exist or is not a file name then return FILE_PATH_NOT_EXIST_OR_NOT_A_FILE.
+     * If targetPath is not exist or is not a directory name then return TARGET_PATH_NOT_EXIST_OR_NOT_A_DIRECTORY.
+     * if the created file was deleted during the process then return CREATED_FILE_DELETED_DURING_COPY_PROCESS.
+     *
+     * @param filePath which is the path of the file to be copied (include file name)
+     * @param targetPath which  the path of the target directory
+     * @param ifTheFileAlreadyExists can be IF_THE_FILE_ALREADY_EXISTS_DO_NOTHING or IF_THE_FILE_ALREADY_EXISTS_CHANGE_FILE_NAME or IF_THE_FILE_ALREADY_EXISTS_OVERWRITE.
+     * @param theNameOfTheCopyFileCreated is file name which will created. If the function fails then it does not matter.
+     * @return It depends on the situation.
      */
-    bool copyFile(string filePath, string TargetPath);
+    int copyFile(string filePath, string targetPath, int ifTheFileAlreadyExists, string* theNameOfTheCopyFileCreated);
+
+    /*
+     * Is given file path a file name
+     *
+     * @param filePath will checked.
+     * @return filePath is a file name then return true. otherwise return false.
+     */
+    bool isItAFile(string filePath);
+
+    /*
+     * Is given directory path a directory name
+     *
+     * @param dirPath will checked.
+     * @return dirPath is a directory name then return true. otherwise return false.
+     */
+    bool isItADirectory(string dirPath);
+
+    /*
+     * Find The File Name In Given Path
+     *
+     * @param filePath need for search
+     * @param theNameOfTheFileToBeCopied will be file name.
+     */
+    void findTheFileNameInGivenPath(string filePath, string* theNameOfTheFileToBeCopied);
+
+    /*
+     * Merge The Given Path And Given File Name
+     *
+     * @param Path is beginning of the newPath
+     * @param fileName is ending of the newPath
+     * @param newPath will created.
+     */
+    void mergethePathAndFileName(string Path, string fileName, string* newPath);
+
+    /*
+     * Copy From Source File To Destination File
+     *
+     * @param sourceFile is source file for copy process
+     * @param destinationFile is destination file for copy process
+     * @return copy fails then return false otherwise return true.
+     */
+    bool copyFromSourceFileToDestinationFile(int sourceFile, int destinationFile);
+
+    /*
+     * Find The Unique Name In The Path
+     *
+     * @param filePath is source path for finding process
+     * @param newFileName will created.
+     * @return is not important
+     */
+    bool findTheUniqueNameInThePath(string* filePath);
 
     /*
      * Get MailBox Object and run it.
