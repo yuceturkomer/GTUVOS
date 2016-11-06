c1ee64a66df73aad62645d209910b18d2e832c6e
hmenn <hmen.56@gmail.com>
2016-11-07 02:41:41 +0300

    listCommand guncellendir
    farklı klasorleri listeleme eklendi
    klasor içindeki klasorler için dir tagi eklendi
    fazla metodlar silindi

--- lscmd.h
+++ lscmd.h
@@ -20,8 +20,7 @@ class ListCMD : public ICommand
 public:
     ~ListCMD();
     ListCMD(QStringList params);
-    void traversepath(Ui::MainWindow* window);
-    int getNumberOfSubDirectory(char *directory);
+    void traversepath(Ui::MainWindow* window,const char* path);
     void execute(Ui::MainWindow *mainWindow);
 
 };
